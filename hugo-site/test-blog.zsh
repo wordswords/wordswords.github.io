ca52ec41e39b67ce22f194fa1f95f2c5552eb2a9
David Craddock <contact@davidcraddock.net>
2025-09-13 03:34:03 +0100

    Might as well have this in here too

--- hugo-site/test-blog.zsh
+++ hugo-site/test-blog.zsh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+find ./content/ -name '*.md' -type f -exec perl -0777 -pi -e 's/\{\{\/\*.*?\*\/\}\}//gs' {} \;
 hugo --gc --config ./hugo.yaml
 hugo server --bind 0.0.0.0 --buildDrafts --config ./hugo.yaml --disableFastRender
 
