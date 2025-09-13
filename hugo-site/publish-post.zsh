6aeb992a9039a74a6ad9f628be0b06e472506736
David Craddock <contact@davidcraddock.net>
2025-09-13 05:41:05 +0100

    Last step in the process

--- hugo-site/publish-post.zsh
+++ hugo-site/publish-post.zsh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-find ./content/ -name '*.md' -type f -exec perl -0777 -pi -e 's/\{\{\/\*.*?\*\/\}\}//gs' {} /;
+find ./content/ -name '*.md' -type f -exec perl -0777 -pi -e 's/\{\{\/\*.*?\*\/\}\}//gs' {} \;
 git add ./content/
 git commit -m "Latest hugo update" || echo ''
 git push origin main
